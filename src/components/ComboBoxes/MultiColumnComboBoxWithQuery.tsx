4d102473a1a4b3c1b1fa588d897703ac38e8a5f1
YUJIN-G <yjgo235@gmail.com>
2022-10-06 17:42:38 +0900

    멀티 컬럼박스 추가

---
+++ src/components/ComboBoxes/MultiColumnComboBoxWithQuery.tsx
@@ -0,0 +1,93 @@
+import React, { useCallback, useEffect, useState } from "react";
+import {
+  MultiColumnComboBox as KendoMultiColumnComboBox,
+  ComboBoxChangeEvent,
+} from "@progress/kendo-react-dropdowns";
+
+import { useApi } from "../../hooks/api";
+
+type TComboBox = {
+  name: string;
+  value: string | number;
+  queryStr: string;
+  columns: any;
+  changeData(e: any): void;
+  textField?: string;
+  valueField?: string;
+};
+const MultiColumnComboBoxWithQuery = ({
+  name,
+  value,
+  queryStr,
+  columns,
+  changeData,
+  textField = "code_name",
+  valueField = "sub_code",
+}: TComboBox) => {
+  const processApi = useApi();
+  const [listData, setListData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  //콤보박스 리스트 쿼리 조회
+  const fetchData = useCallback(async () => {
+    let data: any;
+
+    // const queryStr = customOptionData.menuCustomDefaultOptions.query.find(
+    //   (item: any) => item.id === name
+    // ).query;
+
+    let query = {
+      query: "query?query=" + queryStr,
+    };
+
+    try {
+      data = await processApi<any>("query", query);
+    } catch (error) {
+      data = null;
+    }
+
+    if (data.isSuccess === true) {
+      const rows = data.tables[0].Rows;
+
+      setListData(rows); //리스트 세팅
+    }
+  }, []);
+
+  // const columns = customOptionData.menuCustomDefaultOptions.query.find(
+  //   (item: any) => item.id === name
+  // ).bizComponentItems;
+
+  let newColumns = [];
+
+  if (columns) {
+    newColumns = columns.map((column: any) => ({
+      field: column.fieldName,
+      header: column.caption,
+      width: column.columnWidth,
+    }));
+
+    newColumns = newColumns.filter((column: any) => column.width !== 0);
+  }
+  const onChangeHandle = (e: ComboBoxChangeEvent) => {
+    let value = e.target.value === null ? "" : e.target.value[valueField];
+    changeData({ name, value });
+  };
+
+  return (
+    <KendoMultiColumnComboBox
+      id={name}
+      data={listData}
+      value={
+        value ? listData.find((item: any) => item[valueField] === value) : ""
+      }
+      columns={newColumns}
+      textField={textField}
+      onChange={onChangeHandle}
+    />
+  );
+};
+
+export default MultiColumnComboBoxWithQuery;
