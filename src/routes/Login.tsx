9ea05b716c2b756366f4fde48f5cef4e9701be13
YUJIN-G <yjgo235@gmail.com>
2023-01-30 09:29:14 +0900

    로그인 로직 수정

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -1,32 +1,40 @@
 import { Button } from "@progress/kendo-react-buttons";
 import { Form, Field, FormElement } from "@progress/kendo-react-form";
-import { Input } from "@progress/kendo-react-inputs";
-import { KeyboardEvent, useCallback, useEffect, useState } from "react";
+import { KeyboardEvent, useCallback, useEffect, useRef, useState } from "react";
 import { useHistory } from "react-router-dom";
 import { menusState, tokenState } from "../store/atoms";
 import { useApi } from "../hooks/api";
-import { useRecoilState, useSetRecoilState } from "recoil";
+import { useSetRecoilState } from "recoil";
 import { FormInput, FormComboBox } from "../components/Editors";
 import { LoginBox } from "../CommonStyled";
-import { sha256 } from "js-sha256";
 import { UseGetIp } from "../components/CommonFunction";
 import { isLoading } from "../store/atoms";
 import Loading from "../components/Loading";
 
-interface FormData {
-  companyCode: string;
+const allowedIpAddress = {
+  "gst-busan": "222.96.157.66",
+  "gst-seoul": "125.141.105.80",
+  "gst-vpn": "10.212.134.206",
+};
+
+interface IFormData {
+  langCode: string;
+  companyCode: string | { company_code: string };
   userId: string;
   password: string;
 }
-const Login: React.FC = () => {
-  const [token, setToken] = useRecoilState(tokenState);
-  const [menus, setMenus] = useRecoilState(menusState);
-  //const [api, setApi] = useRecoilState(apiState);
-  const history = useHistory();
-  const processApi = useApi();
-  const setLoading = useSetRecoilState(isLoading);
 
-  const handleSubmit = (data: { [name: string]: FormData }) => {
+const Login: React.FC = () => {
+  const processApi = useApi();
+  const history = useHistory();
+  const setToken = useSetRecoilState(tokenState);
+  const setMenus = useSetRecoilState(menusState);
+  const setLoading = useSetRecoilState(isLoading);
+  const [isAllowedIpAddress, setIsAllowedIpAddress] = useState(false);
+  const [ip, setIp] = useState("");
+  UseGetIp(setIp);
+
+  const handleSubmit = (data: { [name: string]: any }) => {
     processLogin(data);
   };
 
@@ -34,18 +42,29 @@ const Login: React.FC = () => {
     async (formData: { [name: string]: any }) => {
       try {
         setLoading(true);
-        let para = Object.assign({}, formData);
 
-        if (isAllowedIpAddress) {
+        let para: IFormData = Object.assign(
+          {},
+          {
+            langCode: formData.langCode,
+            companyCode: formData.companyCode,
+            userId: formData.userId,
+            password: formData.password,
+          }
+        );
+
+        if (typeof para.companyCode !== "string") {
           para.companyCode = para.companyCode.company_code;
         }
-        //const md5 = require("md5");
-        //para.password = sha256(md5(para.password));
 
-        const response = await processApi<any>(
-          para.companyCode === "2207C612" ? "login-old" : "login",
-          para
-        );
+        // const md5 = require("md5");
+        // para.password = sha256(md5(para.password));
+        // const response = await processApi<any>(
+        //   para.companyCode === "2207C612" ? "login-old" : "login",
+        //   para
+        // );
+
+        const response = await processApi<any>("login", para);
 
         const {
           token,
@@ -71,12 +90,9 @@ const Login: React.FC = () => {
         });
 
         history.replace("/Home");
-
-        //setShowLoading(false);
         setLoading(false);
       } catch (e: any) {
         console.log("login error", e);
-        //setShowLoading(false);
         setLoading(false);
         alert(e.message);
       }
@@ -90,22 +106,18 @@ const Login: React.FC = () => {
     setToken(null as any);
     setMenus(null as any);
   }, []);
-  const [isAllowedIpAddress, setIsAllowedIpAddress] = useState(false);
-  const [ip, setIp] = useState("");
-  UseGetIp(setIp);
 
-  const allowedIpAddress = {
-    "gst-busan": "222.96.157.66",
-    "gst-seoul": "125.141.105.80",
-    "gst-vpn": "10.212.134.206",
-  };
-
-  const handleKeyPress = (e: KeyboardEvent<HTMLDivElement>) => {
-    if (e.key === "'" && Object.values(allowedIpAddress).includes(ip)) {
+  const companyCodeKeyDown = (e: KeyboardEvent<HTMLDivElement>) => {
+    console.log(e);
+    if (
+      e.ctrlKey &&
+      e.key === "'" &&
+      Object.values(allowedIpAddress).includes(ip)
+    ) {
       setIsAllowedIpAddress((prev) => !prev);
-      console.log(1);
     }
   };
+
   return (
     <LoginBox>
       <Form
@@ -126,7 +138,7 @@ const Login: React.FC = () => {
                   ifGetCompanyCode={true}
                   valueField="company_code"
                   textField="name"
-                  onKeyPress={handleKeyPress}
+                  onKeyDown={companyCodeKeyDown}
                   columns={[
                     {
                       sortOrder: 0,
@@ -156,7 +168,7 @@ const Login: React.FC = () => {
                   name={"companyCode"}
                   label={"업체코드"}
                   component={FormInput}
-                  onKeyPress={handleKeyPress}
+                  onKeyDown={companyCodeKeyDown}
                 />
               )}
 
