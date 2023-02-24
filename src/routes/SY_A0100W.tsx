322c1f5c34a78f699b8114ea537ef530f7798c11
YUJIN-G <yjgo235@gmail.com>
2023-02-24 13:03:38 +0900

    데이터등록현황 - 합계셀, 스크롤처리, 조회오류수정

--- src/routes/SY_A0100W.tsx
+++ src/routes/SY_A0100W.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   Grid,
   GridColumn,
@@ -24,27 +24,18 @@ import MonthCalendar from "../components/Calendars/MonthCalendar";
 import {
   chkScrollHandler,
   convertDateToStr,
-  getQueryFromBizComponent,
   setDefaultDate,
-  UseBizComponent,
   UseCustomOption,
   UsePermissions,
   UseGetValueFromSessionItem,
 } from "../components/CommonFunction";
-import { IItemData } from "../hooks/interfaces";
-import {
-  CLIENT_WIDTH,
-  COM_CODE_DEFAULT_VALUE,
-  PAGE_SIZE,
-} from "../components/CommonString";
+import { CLIENT_WIDTH, PAGE_SIZE } from "../components/CommonString";
 import NumberCell from "../components/Cells/NumberCell";
 import { TabStrip, TabStripTab } from "@progress/kendo-react-layout";
 import TopButtons from "../components/TopButtons";
-import { bytesToBase64 } from "byte-base64";
 import { useSetRecoilState } from "recoil";
 import { isLoading } from "../store/atoms";
 import CustomOptionComboBox from "../components/ComboBoxes/CustomOptionComboBox";
-import { TableSelectionChangeEvent } from "@progress/kendo-react-data-tools";
 import { Checkbox } from "@progress/kendo-react-inputs";
 
 const App: React.FC = () => {
@@ -52,6 +43,7 @@ const App: React.FC = () => {
   const processApi = useApi();
   const userId = UseGetValueFromSessionItem("user_id");
   const pathname: string = window.location.pathname.replace("/", "");
+  const [usedUserCnt, setUsedUserCnt] = useState(0);
 
   const [permissions, setPermissions] = useState<TPermissions | null>(null);
   UsePermissions(setPermissions);
@@ -63,80 +55,13 @@ const App: React.FC = () => {
   //customOptionData 조회 후 디폴트 값 세팅
   useEffect(() => {
     if (customOptionData !== null) {
-      const defaultOption = customOptionData.menuCustomDefaultOptions.query;
-
       setFilters((prev) => ({
         ...prev,
         ymdFrdt: setDefaultDate(customOptionData, "ymdFrdt"),
         ymdTodt: setDefaultDate(customOptionData, "ymdTodt"),
-        // cboLocation: defaultOption.find(
-        //   (item: any) => item.id === "cboLocation"
-        // ).valueCode,
-        // radFinyn: defaultOption.find((item: any) => item.id === "radFinyn")
-        //   .valueCode,
       }));
     }
   }, [customOptionData]);
-
-  const [bizComponentData, setBizComponentData] = useState<any>(null);
-  UseBizComponent(
-    "L_QC002,L_PR010,L_fxcode,L_dptcd_001,L_sysUserMaster_001",
-    //불량유형, 공정, 설비, 부서코드, 담당자
-    setBizComponentData
-  );
-
-  //공통코드 리스트 조회
-  const [proccdListData, setProccdListData] = useState([
-    COM_CODE_DEFAULT_VALUE,
-  ]);
-  const [prodmacListData, setProdmacListData] = useState([
-    { fxfull: "", fxcode: "" },
-  ]);
-  const [prodempListData, setProdempListData] = useState([
-    { user_id: "", user_name: "" },
-  ]);
-
-  useEffect(() => {
-    if (bizComponentData !== null) {
-      const proccdQueryStr = getQueryFromBizComponent(
-        bizComponentData.find((item: any) => item.bizComponentId === "L_PR010")
-      );
-      const prodmacQueryStr = getQueryFromBizComponent(
-        bizComponentData.find((item: any) => item.bizComponentId === "L_fxcode")
-      );
-      const prodempQueryStr = getQueryFromBizComponent(
-        bizComponentData.find(
-          (item: any) => item.bizComponentId === "L_sysUserMaster_001"
-        )
-      );
-
-      fetchQuery(proccdQueryStr, setProccdListData);
-      fetchQuery(prodmacQueryStr, setProdmacListData);
-      fetchQuery(prodempQueryStr, setProdempListData);
-    }
-  }, [bizComponentData]);
-
-  const fetchQuery = useCallback(async (queryStr: string, setListData: any) => {
-    let data: any;
-
-    const bytes = require("utf8-bytes");
-    const convertedQueryStr = bytesToBase64(bytes(queryStr));
-
-    let query = {
-      query: convertedQueryStr,
-    };
-
-    try {
-      data = await processApi<any>("query", query);
-    } catch (error) {
-      data = null;
-    }
-
-    if (data.isSuccess === true) {
-      const rows = data.tables[0].Rows;
-      setListData(rows);
-    }
-  }, []);
 
   const [dataState, setDataState] = useState<State>({
     sort: [],
@@ -154,6 +79,7 @@ const App: React.FC = () => {
   const [tabSelected, setTabSelected] = React.useState(0);
   const handleSelectTab = (e: any) => {
     setDataState({});
+    setMainDataResult(process([], dataState));
     setTabSelected(e.selected);
 
     if (e.selected === 0) {
@@ -169,6 +95,7 @@ const App: React.FC = () => {
       setFilters((prev) => ({
         ...prev,
         ifFetch: true,
+        pgNum: 1,
         work_type: "ENTRY",
       }));
     } else if (e.selected === 1) {
@@ -184,6 +111,7 @@ const App: React.FC = () => {
       setFilters((prev) => ({
         ...prev,
         ifFetch: true,
+        pgNum: 1,
         work_type: "usage_log",
       }));
     } else if (e.selected === 2) {
@@ -199,6 +127,7 @@ const App: React.FC = () => {
       setFilters((prev) => ({
         ...prev,
         ifFetch: true,
+        pgNum: 1,
         work_type: "usergroup",
       }));
     }
@@ -241,6 +170,7 @@ const App: React.FC = () => {
   //조회조건 초기값
   const [filters, setFilters] = useState({
     ifFetch: true,
+    pgNum: 1,
     work_type: "ENTRY",
     orgdiv: "01",
     cboLocation: "01",
@@ -258,8 +188,8 @@ const App: React.FC = () => {
   //조회조건 파라미터
   const dataParameters: Iparameters = {
     procedureName: "web_sel_data_entry_log",
-    pageNumber: 0,
-    pageSize: 0,
+    pageNumber: filters.pgNum,
+    pageSize: PAGE_SIZE,
     parameters: {
       "@p_work_type": filters.work_type,
       "@p_orgdiv": filters.orgdiv,
@@ -274,8 +204,22 @@ const App: React.FC = () => {
 
   const programParameters: Iparameters = {
     procedureName: "P_SY_A0100W_Q2",
-    pageNumber: 0,
-    pageSize: 0,
+    pageNumber: filters.pgNum,
+    pageSize: PAGE_SIZE,
+    parameters: {
+      "@p_work_type": filters.work_type,
+      "@p_orgdiv": filters.orgdiv,
+      "@p_location": filters.cboLocation,
+      "@p_yyyymm": convertDateToStr(filters.yyyymm),
+      "@p_is_all_menu": programFilters.is_all_menu,
+      "@p_user_groupping": programFilters.user_groupping,
+    },
+  };
+
+  const userParameters: Iparameters = {
+    procedureName: "P_SY_A0100W_Q2",
+    pageNumber: filters.pgNum,
+    pageSize: PAGE_SIZE,
     parameters: {
       "@p_work_type": filters.work_type,
       "@p_orgdiv": filters.orgdiv,
@@ -291,73 +235,48 @@ const App: React.FC = () => {
     if (!permissions?.view) return;
     let data: any;
     setLoading(true);
+
     try {
-      data = await processApi<any>("procedure", dataParameters);
+      data = await processApi<any>(
+        "procedure",
+        tabSelected === 0
+          ? dataParameters
+          : tabSelected === 1
+          ? programParameters
+          : userParameters
+      );
     } catch (error) {
       data = null;
     }
 
     if (data.isSuccess === true) {
-      const totalRowCnt = data.tables[0].RowCount;
+      const totalRowCnt = data.tables[0].TotalRowCount;
+      const usedUserCnt = data.returnString;
       const useTotalRow = data.tables[1].Rows[0];
       const rows = data.tables[0].Rows.map((item: any) => ({
         ...item,
-        useTotalRow: useTotalRow,
+        useTotalRow,
       }));
 
+      // 실사용자수
+      if (usedUserCnt !== "") setUsedUserCnt(usedUserCnt);
+
       if (totalRowCnt > 0) {
-        setMainDataResult(() => {
+        setMainDataResult((prev) => {
           return {
-            data: [...rows],
+            data: prev.data.concat(rows),
             total: totalRowCnt,
           };
         });
       } else {
-        resetResultData();
+        setMainDataResult(process([], dataState));
       }
     }
+
+    setFilters((prev) => ({ ...prev, ifFetch: false }));
     setLoading(false);
   };
 
-  const fetchProgramGrid = async () => {
-    let data: any;
-    setLoading(true);
-    try {
-      data = await processApi<any>("procedure", programParameters);
-    } catch (error) {
-      data = null;
-    }
-
-    if (data.isSuccess === true) {
-      const totalRowCnt = data.tables[0].RowCount;
-      const useTotalRow = data.tables[1].Rows[0];
-      const rows = data.tables[0].Rows.map((item: any) => ({
-        ...item,
-        useTotalRow: useTotalRow,
-      }));
-
-      if (totalRowCnt > 0) {
-        setMainDataResult(() => {
-          return {
-            data: [...rows],
-            total: totalRowCnt,
-          };
-        });
-      } else {
-        resetResultData();
-      }
-    }
-    setLoading(false);
-  };
-
-  const resetResultData = () => {
-    setMainDataResult(() => {
-      return {
-        data: [],
-        total: 0,
-      };
-    });
-  };
   //엑셀 내보내기
   let _export: ExcelExport | null | undefined;
   const exportExcel = () => {
@@ -378,6 +297,7 @@ const App: React.FC = () => {
       </td>
     );
   };
+
   const TitleFooterCell = (props: GridFooterCellProps) => {
     return (
       <td colSpan={props.colSpan} style={props.style}>
@@ -388,35 +308,40 @@ const App: React.FC = () => {
       </td>
     );
   };
+  const UsedUserFooterCell = (props: GridFooterCellProps) => {
+    return (
+      <td colSpan={props.colSpan} style={props.style}>
+        기준사용자 {usedUserCnt}명
+      </td>
+    );
+  };
   const DateFooterCell = (props: GridFooterCellProps) => {
     const { field = "" } = props;
-    let fieldDate = field.replace("use_cnt_", "");
+    let fieldDate = field.replace(
+      tabSelected === 0 ? "data_cnt_" : "use_cnt_",
+      ""
+    );
     let dayTotal = 0;
     let useTotal = 0;
     let percentage = 0;
 
-    if (fieldDate !== "tt") {
-      fieldDate = Number(fieldDate) < 10 ? "0" + fieldDate : fieldDate;
-    }
     mainDataResult.data.forEach((item) => {
       dayTotal = dayTotal + (item[field] ?? 0);
     });
 
     if (mainDataResult.data.length > 0) {
       const useTotalDataResult = mainDataResult.data[0]["useTotalRow"];
-
       useTotal =
         (fieldDate && useTotalDataResult ? useTotalDataResult[fieldDate] : 0) ??
         0;
-
-      percentage = (useTotalDataResult["tt"] / useTotal) * 100;
+      percentage = (useTotal / usedUserCnt) * 100;
     }
 
     return (
       <td colSpan={props.colSpan} style={props.style}>
         {dayTotal} <br />
         {useTotal} <br />
-        {percentage}
+        {Math.round(percentage * 10) / 10}
       </td>
     );
   };
@@ -434,23 +359,21 @@ const App: React.FC = () => {
     setDataState((prev) => ({ ...prev, sort: e.sort }));
   };
 
-  //조회조건 사용자 옵션 디폴트 값 세팅 후 최초 한번만 실행
+  // 조회
   useEffect(() => {
     if (
       filters.ifFetch === true &&
       customOptionData !== null &&
       permissions !== null
     ) {
-      search();
-      setFilters((prev) => ({ ...prev, ifFetch: false }));
+      fetchDataGrid();
     }
   }, [filters, permissions]);
 
-  const search = () => {
-    if (tabSelected === 0) {
-      fetchDataGrid();
-    } else {
-      fetchProgramGrid();
+  //스크롤 핸들러 => 한번에 pageSize만큼 조회
+  const onScrollHandler = (event: GridEvent) => {
+    if (chkScrollHandler(event, filters.pgNum, PAGE_SIZE)) {
+      setFilters((prev) => ({ ...prev, ifFetch: true, pgNum: prev.pgNum + 1 }));
     }
   };
 
@@ -462,28 +385,18 @@ const App: React.FC = () => {
           data={process(
             mainDataResult.data.map((row: any) => ({
               ...row,
-              proccd: proccdListData.find(
-                (item: any) => item.sub_code === row.proccd
-              )?.code_name,
-              prodmac: prodmacListData.find(
-                (item: any) => item.fxcode === row.prodmac
-              )?.fxfull,
-              prodemp: prodempListData.find(
-                (item: any) => item.user_id === row.prodemp
-              )?.user_name,
-              badpct: Math.round(row.badpct),
             })),
             dataState
           )}
           {...dataState}
           onDataStateChange={onDataStateChange}
-          //정렬기능
-          sortable={true}
-          onSortChange={onSortChange}
           //스크롤 조회 기능
           fixedScroll={true}
           total={mainDataResult.total}
-          //onScroll={onScrollHandler}
+          onScroll={onScrollHandler}
+          //정렬기능
+          sortable={true}
+          onSortChange={onSortChange}
           //컬럼순서조정
           reorderable={true}
           //컬럼너비조정
@@ -504,14 +417,18 @@ const App: React.FC = () => {
           <GridColumn
             field="form_id"
             title="프로그램ID"
-            width="120px"
-            footerCell={tabSelected !== 2 ? TotalFooterCell : undefined}
+            width="140px"
+            footerCell={
+              tabSelected !== 2 ? TotalFooterCell : UsedUserFooterCell
+            }
           />
           <GridColumn
             field="menu_name"
             title="프로그램명"
-            width="120px"
-            footerCell={tabSelected === 2 ? TitleFooterCell : undefined}
+            width="140px"
+            footerCell={
+              tabSelected === 2 ? TitleFooterCell : UsedUserFooterCell
+            }
           />
           {tabSelected !== 2 && (
             <GridColumn
@@ -521,9 +438,9 @@ const App: React.FC = () => {
               footerCell={TitleFooterCell}
             />
           )}
-          <GridColumn title="일자">{createColumn()}</GridColumn>
+          <GridColumn title="일자">{createDateColumn()}</GridColumn>
           <GridColumn
-            field="use_cnt_tt"
+            field={tabSelected === 0 ? "data_cnt_tt" : "use_cnt_tt"}
             title="합계"
             width="120px"
             footerCell={DateFooterCell}
@@ -533,14 +450,15 @@ const App: React.FC = () => {
     );
   };
 
-  const createColumn = () => {
+  const createDateColumn = () => {
     const array = [];
     for (var i = 1; i <= 31; i++) {
+      const num = i < 10 ? "0" + i : i + "";
       array.push(
         <GridColumn
           key={i}
-          field={"use_cnt_" + i}
-          title={i < 10 ? "0" + i : i + ""}
+          field={(tabSelected === 0 ? "data_cnt_" : "use_cnt_") + num}
+          title={num}
           width="50px"
           cell={NumberCell}
           footerCell={DateFooterCell}
@@ -558,7 +476,10 @@ const App: React.FC = () => {
         <ButtonContainer>
           {permissions && (
             <TopButtons
-              search={search}
+              search={() => {
+                setMainDataResult(process([], dataState)); // reset
+                setFilters((prev) => ({ ...prev, ifFetch: true, pgNum: 1 })); //조회
+              }}
               exportExcel={exportExcel}
               permissions={permissions}
             />
