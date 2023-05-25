db4a6f1e564cbb69c36d23dcee5aed20027ee857
서지연 <joyn601@naver.com>
2023-05-26 08:40:44 +0900

    수정

---
+++ src/routes/HU_A1000W.tsx
@@ -0,0 +1,92 @@
+import React, { useCallback, useEffect, useState, useRef } from "react";
+import * as ReactDOM from "react-dom";
+import {
+  Grid,
+  GridColumn,
+  GridDataStateChangeEvent,
+  GridEvent,
+  GridSelectionChangeEvent,
+  getSelectedState,
+  GridFooterCellProps,
+  GridCellProps,
+} from "@progress/kendo-react-grid";
+import { DatePicker } from "@progress/kendo-react-dateinputs";
+import { ExcelExport } from "@progress/kendo-react-excel-export";
+import { Icon, getter } from "@progress/kendo-react-common";
+import { DataResult, process, State } from "@progress/kendo-data-query";
+import calculateSize from "calculate-size";
+import FilterContainer from "../components/Containers/FilterContainer";
+import {
+  Title,
+  FilterBox,
+  GridContainer,
+  GridTitle,
+  TitleContainer,
+  ButtonContainer,
+  GridTitleContainer,
+  ButtonInInput,
+} from "../CommonStyled";
+import { Button } from "@progress/kendo-react-buttons";
+import { Input } from "@progress/kendo-react-inputs";
+import { useApi } from "../hooks/api";
+import { Iparameters, TPermissions } from "../store/types";
+import {
+  chkScrollHandler,
+  convertDateToStr,
+  findMessage,
+  getQueryFromBizComponent,
+  setDefaultDate,
+  UseBizComponent,
+  UseCustomOption,
+  UseMessages,
+  UsePermissions,
+  handleKeyPressSearch,
+  UseParaPc,
+  UseGetValueFromSessionItem,
+  useSysMessage,
+} from "../components/CommonFunction";
+import CustomersWindow from "../components/Windows/CommonWindows/CustomersWindow";
+import ItemsWindow from "../components/Windows/CommonWindows/ItemsWindow";
+import DateCell from "../components/Cells/DateCell";
+import NumberCell from "../components/Cells/NumberCell";
+import {
+  COM_CODE_DEFAULT_VALUE,
+  PAGE_SIZE,
+  SELECTED_FIELD,
+} from "../components/CommonString";
+import CustomOptionRadioGroup from "../components/RadioGroups/CustomOptionRadioGroup";
+import CustomOptionComboBox from "../components/ComboBoxes/CustomOptionComboBox";
+import TopButtons from "../components/Buttons/TopButtons";
+import { deletedAttadatnumsState, isLoading } from "../store/atoms";
+
+const HU_A1000W: React.FC = () => {
+
+  return (
+    <>
+      <TitleContainer>
+        <Title>인사관리</Title>
+      </TitleContainer>
+
+      <FilterContainer>
+        <FilterBox>
+          <tbody>
+            <tr>
+              <th> 부서코드 </th>
+                  <td>
+                  </td>
+
+              <th>성명</th>
+                  <td>
+                  </td>
+              <th>재직여부</th>
+                  <td>
+                  </td>
+            </tr>
+          </tbody>
+        </FilterBox>
+      </FilterContainer>
+    </>
+  );
+};
+
+export default HU_A1000W;
