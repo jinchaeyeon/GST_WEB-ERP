656e19bfda56b47e8050eedaa419d6b691d5e0d3
정라희 <rahee200764@gmail.com>
2023-10-17 13:12:30 +0900

    컨설팅 답변관리 1차 제작 (api 500 오류사항 수정 필요)

---
+++ src/store/columns/CM_A5001W_C.ts
@@ -0,0 +1,57 @@
+import { TGrid } from "../types";
+
+export const gridList: Array<TGrid> = [
+  {
+    gridName: "grdList",
+    columns: [
+      {
+        id: "col_status",
+        field: "status",
+        caption: "상태",
+        width: 120,
+      },
+      {
+        id: "col_request_date",
+        field: "request_date",
+        caption: "문의일",
+        width: 120,
+      },
+      {
+        id: "col_finexpdt",
+        field: "finexpdt",
+        caption: "답변기한요청일",
+        width: 120,
+      },
+      {
+        id: "col_completion_date",
+        field: "completion_date",
+        caption: "답변완료일",
+        width: 120,
+      },
+      {
+        id: "col_medicine_type",
+        field: "medicine_type",
+        caption: "의약품상세분류",
+        width: 120,
+      },
+      {
+        id: "col_user_name",
+        field: "user_name",
+        caption: "SM담당자",
+        width: 120,
+      },
+      {
+        id: "col_customer_code",
+        field: "customer_code",
+        caption: "회사명",
+        width: 120,
+      },
+      {
+        id: "col_title",
+        field: "title",
+        caption: "제목",
+        width: 120,
+      },
+    ],
+  },
+];
