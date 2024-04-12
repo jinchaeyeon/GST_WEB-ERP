edec2f528177ef51f26ccc69e00f70a1d0cfc5a8
진채연 <jinchaeyeon@naver.com>
2024-04-12 15:29:24 +0900

    로그인 에러 수정

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,9 @@ import "primeflex/primeflex.css";
 import "primeicons/primeicons.css"; // icons
 import "primereact/resources/primereact.css"; // core css
 import "primereact/resources/themes/lara-light-indigo/theme.css"; // theme
-import React from "react";
 import "react-app-polyfill/stable";
 import { ThemeSwitcherProvider } from "react-css-theme-switcher";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
 import { BrowserRouter } from "react-router-dom";
 import { RecoilRoot } from "recoil";
@@ -14,13 +13,14 @@ import APPWEBERP from "./APPWEBERP";
 import "./flags.css";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
- 
+
+const root = ReactDOM.createRoot(document.getElementById("root")!);
 const path = window.location.href;
 const themes = {
   blue: `WEBERP.css`,
   yellow: `DDGD.css`,
 };
- 
+
 const defaultTheme = path.includes("localhost")
   ? //WEB ERP개발할떄 바꿀부분입니다.
     //"yellow"
@@ -29,23 +29,22 @@ const defaultTheme = path.includes("localhost")
   ? "yellow"
   : "blue";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <ThemeSwitcherProvider
-      themeMap={themes}
-      defaultTheme={defaultTheme}
-      insertionPoint="styles-insertion-point"
-    >
-      <RecoilRoot>
-        <HelmetProvider>
-          <BrowserRouter>
-            {defaultTheme == "yellow" ? <APPDDGD /> : <APPWEBERP />}
-          </BrowserRouter>
-        </HelmetProvider>
-      </RecoilRoot>
-    </ThemeSwitcherProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+root.render(
+  //<React.StrictMode>
+  <ThemeSwitcherProvider
+    themeMap={themes}
+    defaultTheme={defaultTheme}
+    insertionPoint="styles-insertion-point"
+  >
+    <RecoilRoot>
+      <HelmetProvider>
+        <BrowserRouter>
+          {defaultTheme == "yellow" ? <APPDDGD /> : <APPWEBERP />}
+        </BrowserRouter>
+      </HelmetProvider>
+    </RecoilRoot>
+  </ThemeSwitcherProvider>
+  //</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
