45af04594cff7c68daa19e2cbb24db78610cb149
진채연 <jinchaeyeon@naver.com>
2024-05-02 10:01:22 +0900

    pdf뷰어

--- src/components/Viewer/FileViewers.js
+++ src/components/Viewer/FileViewers.js
@@ -1,13 +1,16 @@
-import { Viewer } from "@react-pdf-viewer/core";
+import { Viewer, Worker } from "@react-pdf-viewer/core";
 import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
 
 import "@react-pdf-viewer/core/lib/styles/index.css";
 import "@react-pdf-viewer/default-layout/lib/styles/index.css";
 
 export default function FileViewers(props) {
+  // const defaultLayoutPluginInstance = defaultLayoutPlugin();
   const defaultLayoutPluginInstance = defaultLayoutPlugin();
 
   return (
-    <Viewer fileUrl={props.file} plugins={[defaultLayoutPluginInstance]} />
+    <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+      <Viewer fileUrl={props.file} plugins={[defaultLayoutPluginInstance]} />
+    </Worker>
   );
 }
