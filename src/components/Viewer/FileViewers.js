6cfcf13191aff47fbe31eb4f71f4882ddd56e1a3
진채연 <jinchaeyeon@naver.com>
2024-05-02 11:44:39 +0900

    버전 수정

--- src/components/Viewer/FileViewers.js
+++ src/components/Viewer/FileViewers.js
@@ -5,11 +5,12 @@ import "@react-pdf-viewer/core/lib/styles/index.css";
 import "@react-pdf-viewer/default-layout/lib/styles/index.css";
 
 export default function FileViewers(props) {
-  // const defaultLayoutPluginInstance = defaultLayoutPlugin();
   const defaultLayoutPluginInstance = defaultLayoutPlugin();
 
   return (
-    <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+    <Worker
+      workerUrl={`https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js`}
+    >
       <Viewer fileUrl={props.file} plugins={[defaultLayoutPluginInstance]} />
     </Worker>
   );
