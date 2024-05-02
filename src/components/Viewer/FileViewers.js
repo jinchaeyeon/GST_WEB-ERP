958949e8d694c9d1f2f56cf484f6c7ad2818c2de
진채연 <jinchaeyeon@naver.com>
2024-05-02 09:00:38 +0900

    사용안하는 파일로직 제외

--- src/components/Viewer/FileViewers.js
+++ src/components/Viewer/FileViewers.js
@@ -1,54 +1,13 @@
-import { Viewer, Worker } from "@react-pdf-viewer/core";
+import { Viewer } from "@react-pdf-viewer/core";
 import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
 
 import "@react-pdf-viewer/core/lib/styles/index.css";
 import "@react-pdf-viewer/default-layout/lib/styles/index.css";
 
 export default function FileViewers(props) {
-  const renderToolbar = (Toolbar) => (
-    <Toolbar>
-      {(slots) => {
-        const { ZoomOut } = slots;
-        return (
-          <div
-            style={{
-              alignItems: "center",
-              display: "flex",
-            }}
-          >
-            <div style={{ padding: "0px 2px" }}>
-              <ZoomOut>
-                {(props) => (
-                  <button
-                    style={{
-                      backgroundColor: "#357edd",
-                      border: "none",
-                      borderRadius: "4px",
-                      color: "#ffffff",
-                      cursor: "pointer",
-                      padding: "8px",
-                    }}
-                    onClick={props.onClick}
-                  >
-                    Zoom out
-                  </button>
-                )}
-              </ZoomOut>
-            </div>
-            ...
-          </div>
-        );
-      }}
-    </Toolbar>
-  );
-
   const defaultLayoutPluginInstance = defaultLayoutPlugin();
 
   return (
-    <Worker
-      workerUrl={`https://unpkg.com/pdfjs-dist@2.16.105/build/pdf.worker.min.js`}
-    >
-      <Viewer fileUrl={props.file} plugins={[defaultLayoutPluginInstance]} />
-    </Worker>
+    <Viewer fileUrl={props.file} plugins={[defaultLayoutPluginInstance]} />
   );
 }
