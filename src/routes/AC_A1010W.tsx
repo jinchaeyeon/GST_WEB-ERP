6d59542a202998b7664fb0a43cee65d91df7eeed
진채연 <jinchaeyeon@naver.com>
2024-07-30 14:09:52 +0900

    견적관리 최종견적금액 해제

--- src/routes/AC_A1010W.tsx
+++ src/routes/AC_A1010W.tsx
@@ -1,4 +1,7 @@
-import React, { useState } from "react";
+import { DataResult, getter, process, State } from "@progress/kendo-data-query";
+import { DatePicker } from "@progress/kendo-react-dateinputs";
+import { GridPageChangeEvent } from "@progress/kendo-react-grid";
+import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
 import {
   ButtonContainer,
   FilterBox,
@@ -6,11 +9,17 @@ import {
   TitleContainer,
 } from "../CommonStyled";
 import TopButtons from "../components/Buttons/TopButtons";
+import MonthCalendar from "../components/Calendars/MonthCalendar";
 import {
   convertDateToStr,
   findMessage,
+  getDeviceHeight,
+  getHeight,
   getMenuName,
+  GetPropertyValueByName,
   handleKeyPressSearch,
+  setDefaultDate,
+  UseCustomOption,
   UseGetValueFromSessionItem,
   UseMessages,
   UsePermissions,
@@ -18,7 +27,71 @@ import {
 import { PAGE_SIZE } from "../components/CommonString";
 import FilterContainer from "../components/Containers/FilterContainer";
 import { TPermissions } from "../store/types";
+
+const DATA_ITEM_KEY = "num";
+var height = 0;
+var height2 = 0;
+
 const AC_A1010W: React.FC = () => {
+  let deviceWidth = document.documentElement.clientWidth;
+  const [isMobile, setIsMobile] = useState(deviceWidth <= 1200);
+  const [mobileheight, setMobileHeight] = useState(0);
+  const [webheight, setWebHeight] = useState(0);
+  const idGetter = getter(DATA_ITEM_KEY);
+  //커스텀 옵션 조회
+  const [customOptionData, setCustomOptionData] = React.useState<any>(null);
+  UseCustomOption(setCustomOptionData);
+  useLayoutEffect(() => {
+    if (customOptionData !== null) {
+      height = getHeight(".TitleContainer");
+      height2 = getHeight(".ButtonContainer");
+
+      const handleWindowResize = () => {
+        let deviceWidth = document.documentElement.clientWidth;
+        setIsMobile(deviceWidth <= 1200);
+        setMobileHeight(getDeviceHeight(true) - height - height2);
+        setWebHeight(getDeviceHeight(true) - height - height2);
+      };
+      handleWindowResize();
+      window.addEventListener("resize", handleWindowResize);
+      return () => {
+        window.removeEventListener("resize", handleWindowResize);
+      };
+    }
+  }, [customOptionData, webheight]);
+  let gridRef: any = useRef(null);
+  //customOptionData 조회 후 디폴트 값 세팅
+  useEffect(() => {
+    if (customOptionData !== null) {
+      const defaultOption = GetPropertyValueByName(
+        customOptionData.menuCustomDefaultOptions,
+        "query"
+      );
+      setFilters((prev) => ({
+        ...prev,
+        yyyymm: setDefaultDate(customOptionData, "yyyymm"),
+        isSearch: true,
+      }));
+    }
+  }, [customOptionData]);
+  const initialPageState = { skip: 0, take: PAGE_SIZE };
+  const [page, setPage] = useState(initialPageState);
+  const pageChange = (event: GridPageChangeEvent) => {
+    const { page } = event;
+
+    setFilters((prev) => ({
+      ...prev,
+      pgNum: Math.floor(page.skip / initialPageState.take) + 1,
+      find_row_value: "",
+      isSearch: true,
+    }));
+
+    setPage({
+      skip: page.skip,
+      take: initialPageState.take,
+    });
+  };
+
   //메시지 조회
   const [messagesData, setMessagesData] = React.useState<any>(null);
   UseMessages(setMessagesData);
@@ -30,6 +103,23 @@ const AC_A1010W: React.FC = () => {
   });
   UsePermissions(setPermissions);
   const sessionOrgdiv = UseGetValueFromSessionItem("orgdiv");
+
+  const [mainDataState, setMainDataState] = useState<State>({
+    sort: [],
+  });
+  const [tempState, setTempState] = useState<State>({
+    sort: [],
+  });
+  const [mainDataResult, setMainDataResult] = useState<DataResult>(
+    process([], mainDataState)
+  );
+  const [tempResult, setTempResult] = useState<DataResult>(
+    process([], tempState)
+  );
+  const [selectedState, setSelectedState] = useState<{
+    [id: string]: boolean | number[];
+  }>({});
+
   const [filters, setFilters] = useState({
     pgSize: PAGE_SIZE,
     orgdiv: sessionOrgdiv,
@@ -38,6 +128,15 @@ const AC_A1010W: React.FC = () => {
     pgNum: 1,
     isSearch: false,
   });
+
+  const filterInputChange = (e: any) => {
+    const { value, name } = e.target;
+
+    setFilters((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
 
   const search = () => {
     try {
@@ -82,6 +181,20 @@ const AC_A1010W: React.FC = () => {
         <FilterBox onKeyPress={(e) => handleKeyPressSearch(e, search)}>
           <tbody>
             <tr>
+              <th>기준년월</th>
+              <td>
+                <DatePicker
+                  name="yyyymm"
+                  value={filters.yyyymm}
+                  format="yyyy-MM"
+                  onChange={filterInputChange}
+                  className="required"
+                  placeholder=""
+                  calendar={MonthCalendar}
+                />
+              </td>
+              <th></th>
+              <td></td>
             </tr>
           </tbody>
         </FilterBox>
