58fd2e464ade8eb7d6da40e5e85a326f755916c2
진채연 <jinchaeyeon@naver.com>
2024-08-02 17:15:28 +0900

    업체별월별지시집계 제작완료

---
+++ src/routes/CM_B1105W.tsx
@@ -0,0 +1,512 @@
+import { DataResult, getter, process, State } from "@progress/kendo-data-query";
+import { DatePicker } from "@progress/kendo-react-dateinputs";
+import { ExcelExport } from "@progress/kendo-react-excel-export";
+import {
+  getSelectedState,
+  Grid,
+  GridCellProps,
+  GridColumn,
+  GridDataStateChangeEvent,
+  GridFooterCellProps,
+  GridPageChangeEvent,
+  GridSelectionChangeEvent,
+} from "@progress/kendo-react-grid";
+import React, { useEffect, useLayoutEffect, useState } from "react";
+import { useSetRecoilState } from "recoil";
+import "swiper/css";
+import {
+  ButtonContainer,
+  FilterBox,
+  GridContainer,
+  GridTitle,
+  GridTitleContainer,
+  Title,
+  TitleContainer,
+} from "../CommonStyled";
+import TopButtons from "../components/Buttons/TopButtons";
+import MonthCalendar from "../components/Calendars/MonthCalendar";
+import CustomOptionComboBox from "../components/ComboBoxes/CustomOptionComboBox";
+import {
+  convertDateToStr,
+  findMessage,
+  getBizCom,
+  getDeviceHeight,
+  getHeight,
+  getMenuName,
+  GetPropertyValueByName,
+  handleKeyPressSearch,
+  setDefaultDate,
+  UseBizComponent,
+  UseCustomOption,
+  UseGetValueFromSessionItem,
+  UseMessages,
+  UsePermissions,
+} from "../components/CommonFunction";
+import { PAGE_SIZE, SELECTED_FIELD } from "../components/CommonString";
+import FilterContainer from "../components/Containers/FilterContainer";
+import { useApi } from "../hooks/api";
+import { isLoading } from "../store/atoms";
+import { Iparameters, TPermissions } from "../store/types";
+
+const DATA_ITEM_KEY = "num";
+var height = 0;
+var height2 = 0;
+
+const CM_B1105W: React.FC = () => {
+  const [permissions, setPermissions] = useState<TPermissions>({
+    save: false,
+    print: false,
+    view: false,
+    delete: false,
+  });
+  UsePermissions(setPermissions);
+  const processApi = useApi();
+  const setLoading = useSetRecoilState(isLoading);
+  const idGetter = getter(DATA_ITEM_KEY);
+  //커스텀 옵션 조회
+  const [messagesData, setMessagesData] = React.useState<any>(null);
+  UseMessages(setMessagesData);
+  const [customOptionData, setCustomOptionData] = React.useState<any>(null);
+  UseCustomOption(setCustomOptionData);
+
+  const [bizComponentData, setBizComponentData] = useState<any>(null);
+  UseBizComponent(
+    "L_sysUserMaster_001",
+    //공정, 관리항목리스트
+    setBizComponentData
+  );
+  const [userListData, setUserListData] = useState([
+    { user_id: "", user_name: "" },
+  ]);
+  useEffect(() => {
+    if (bizComponentData !== null) {
+      setUserListData(getBizCom(bizComponentData, "L_sysUserMaster_001"));
+    }
+  }, [bizComponentData]);
+
+  useEffect(() => {
+    if (customOptionData !== null) {
+      const defaultOption = GetPropertyValueByName(
+        customOptionData.menuCustomDefaultOptions,
+        "query"
+      );
+
+      setFilters((prev) => ({
+        ...prev,
+        finexpdt: setDefaultDate(customOptionData, "frdt"),
+        dptcd: defaultOption.find((item: any) => item.id == "dptcd")?.valueCode,
+        person: defaultOption.find((item: any) => item.id == "person")
+          ?.valueCode,
+        isSearch: true,
+      }));
+    }
+  }, [customOptionData]);
+
+  let deviceWidth = document.documentElement.clientWidth;
+  const [isMobile, setIsMobile] = useState(deviceWidth <= 1200);
+  const [mobileheight, setMobileHeight] = useState(0);
+  const [webheight, setWebHeight] = useState(0);
+  useLayoutEffect(() => {
+    if (customOptionData !== null) {
+      height = getHeight(".ButtonContainer");
+      height2 = getHeight(".TitleContainer");
+
+      const handleWindowResize = () => {
+        let deviceWidth = document.documentElement.clientWidth;
+        setIsMobile(deviceWidth <= 1200);
+        setMobileHeight(getDeviceHeight(true) - height - height2);
+        setWebHeight(getDeviceHeight(true) - height - height2);
+      };
+      handleWindowResize();
+      window.addEventListener("resize", handleWindowResize);
+      return () => {
+        window.removeEventListener("resize", handleWindowResize);
+      };
+    }
+  }, [customOptionData, webheight]);
+
+  let _export: any;
+  const exportExcel = () => {
+    if (_export !== null && _export !== undefined) {
+      const optionsGridOne = _export.workbookOptions();
+      optionsGridOne.sheets[0].title = "요약정보";
+      _export.save(optionsGridOne);
+    }
+  };
+  const initialPageState = { skip: 0, take: PAGE_SIZE };
+  const [page, setPage] = useState(initialPageState);
+  const pageChange = (event: GridPageChangeEvent) => {
+    const { page } = event;
+
+    setFilters((prev) => ({
+      ...prev,
+      pgNum: Math.floor(page.skip / initialPageState.take) + 1,
+      find_row_value: "",
+      isSearch: true,
+    }));
+
+    setPage({
+      skip: page.skip,
+      take: initialPageState.take,
+    });
+  };
+  //그리드 리셋
+  const resetAllGrid = () => {
+    setMainDataResult(process([], mainDataState));
+  };
+
+  const search = () => {
+    try {
+      if (convertDateToStr(filters.finexpdt).substring(0, 4) < "1997") {
+        throw findMessage(messagesData, "CM_B1101W_001");
+      } else {
+        resetAllGrid();
+        setPage(initialPageState); // 페이지 초기화
+        setFilters((prev: any) => ({
+          ...prev,
+          pgNum: 1,
+          find_row_value: "",
+          isSearch: true,
+        }));
+      }
+    } catch (e) {
+      alert(e);
+    }
+  };
+
+  const sessionOrgdiv = UseGetValueFromSessionItem("orgdiv");
+  const [filters, setFilters] = useState({
+    pgSize: PAGE_SIZE,
+    orgdiv: sessionOrgdiv,
+    finexpdt: new Date(),
+    dptcd: "",
+    person: "",
+    find_row_value: "",
+    pgNum: 1,
+    isSearch: false,
+  });
+
+  const filterInputChange = (e: any) => {
+    const { value, name } = e.target;
+
+    setFilters((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const filterComboBoxChange = (e: any) => {
+    const { name, value } = e;
+
+    setFilters((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const [mainDataState, setMainDataState] = useState<State>({
+    sort: [],
+  });
+  const [mainDataResult, setMainDataResult] = useState<DataResult>(
+    process([], mainDataState)
+  );
+  const [selectedState, setSelectedState] = useState<{
+    [id: string]: boolean | number[];
+  }>({});
+
+  const [weekendList, setWeekendList] = useState([]);
+
+  //그리드 데이터 조회
+  const fetchMainGrid = async (filters: any) => {
+    if (!permissions.view) return;
+    let data: any;
+    setLoading(true);
+    //조회조건 파라미터
+    const parameters: Iparameters = {
+      procedureName: "P_CM_B1105W_Q",
+      pageNumber: filters.pgNum,
+      pageSize: filters.pgSize,
+      parameters: {
+        "@p_work_type": "LIST",
+        "@p_orgdiv": filters.orgdiv,
+        "@p_finexpdt": convertDateToStr(filters.finexpdt).substring(0, 6),
+        "@p_dptcd": filters.dptcd,
+        "@p_person": filters.person,
+      },
+    };
+
+    try {
+      data = await processApi<any>("procedure", parameters);
+    } catch (error) {
+      data = null;
+    }
+
+    if (data.isSuccess == true) {
+      const totalRowCnt = data.tables[0].TotalRowCount;
+      const rows = data.tables[0].Rows;
+      const rows2 = data.tables[1].Rows;
+
+      setMainDataResult({
+        data: rows,
+        total: totalRowCnt == -1 ? 0 : totalRowCnt,
+      });
+      setWeekendList(rows2);
+      if (totalRowCnt > 0) {
+        setSelectedState({ [rows[0][DATA_ITEM_KEY]]: true });
+      }
+    }
+    setFilters((prev) => ({
+      ...prev,
+      pgNum:
+        data && data.hasOwnProperty("pageNumber")
+          ? data.pageNumber
+          : prev.pgNum,
+      isSearch: false,
+    }));
+    setLoading(false);
+  };
+
+  //조회조건 사용자 옵션 디폴트 값 세팅 후 최초 한번만 실행
+  useEffect(() => {
+    if (
+      filters.isSearch &&
+      permissions.view &&
+      customOptionData !== null &&
+      bizComponentData !== null
+    ) {
+      const _ = require("lodash");
+      const deepCopiedFilters = _.cloneDeep(filters);
+      setFilters((prev) => ({
+        ...prev,
+        find_row_value: "",
+        isSearch: false,
+      })); // 한번만 조회되도록
+      fetchMainGrid(deepCopiedFilters);
+    }
+  }, [filters, permissions, customOptionData, bizComponentData]);
+
+  const onMainDataStateChange = (event: GridDataStateChangeEvent) => {
+    setMainDataState(event.dataState);
+  };
+
+  //그리드 푸터
+  const mainTotalFooterCell = (props: GridFooterCellProps) => {
+    var parts = mainDataResult.total.toString().split(".");
+    return (
+      <td colSpan={props.colSpan} style={props.style}>
+        총
+        {mainDataResult.total == -1
+          ? 0
+          : parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",") +
+            (parts[1] ? "." + parts[1] : "")}
+        건
+      </td>
+    );
+  };
+
+  const onMainSortChange = (e: any) => {
+    setMainDataState((prev) => ({ ...prev, sort: e.sort }));
+  };
+
+  //메인 그리드 선택 이벤트 => 디테일1 그리드 조회
+  const onMainSelectionChange = (event: GridSelectionChangeEvent) => {
+    const newSelectedState = getSelectedState({
+      event,
+      selectedState: selectedState,
+      dataItemKey: DATA_ITEM_KEY,
+    });
+
+    setSelectedState(newSelectedState);
+  };
+
+  const TimeList = () => {
+    let array: any[] = [];
+
+    for (var i = 1; i <= 31; i++) {
+      if (i < 10) {
+        array.push(
+          <GridColumn
+            field={`time_0${i}`}
+            title={`${i}`}
+            width="80px"
+            cell={CustomCell}
+          />
+        );
+      } else {
+        array.push(
+          <GridColumn
+            field={`time_${i}`}
+            title={`${i}`}
+            width="80px"
+            cell={CustomCell}
+          />
+        );
+      }
+    }
+
+    return array;
+  };
+
+  const CustomCell = (props: GridCellProps) => {
+    const {
+      ariaColumnIndex,
+      columnIndex,
+      dataItem,
+      field = "",
+      render,
+      onChange,
+      className = "",
+    } = props;
+    const value = field && dataItem[field] ? dataItem[field] : "";
+
+    let valid = 0;
+    weekendList.map((item: any) => {
+      if (item.date == field.replace("time_", "")) {
+        if (item.weekday == 7) {
+          valid = 7;
+        }
+        if (item.weekday == 1) {
+          valid = 1;
+        }
+      }
+    });
+    return (
+      <td
+        className={className}
+        aria-colindex={ariaColumnIndex}
+        data-grid-col-index={columnIndex}
+        style={{
+          backgroundColor:
+            valid == 7 ? "#d2d2ff" : valid == 1 ? "#ffd2d2" : undefined,
+        }}
+      >
+        {value}
+      </td>
+    );
+  };
+
+  return (
+    <>
+      <TitleContainer className="TitleContainer">
+        <Title>{getMenuName()}</Title>
+
+        <ButtonContainer>
+          {permissions && (
+            <TopButtons
+              search={search}
+              exportExcel={exportExcel}
+              permissions={permissions}
+            />
+          )}
+        </ButtonContainer>
+      </TitleContainer>
+      <FilterContainer>
+        <FilterBox onKeyPress={(e) => handleKeyPressSearch(e, search)}>
+          <tbody>
+            <tr>
+              <th>완료예정일</th>
+              <td>
+                <DatePicker
+                  name="finexpdt"
+                  value={filters.finexpdt}
+                  format="yyyy-MM"
+                  onChange={filterInputChange}
+                  placeholder=""
+                  calendar={MonthCalendar}
+                  className="required"
+                />
+              </td>
+              <th>부서</th>
+              <td>
+                {customOptionData !== null && (
+                  <CustomOptionComboBox
+                    name="dptcd"
+                    value={filters.dptcd}
+                    customOptionData={customOptionData}
+                    changeData={filterComboBoxChange}
+                    textField="dptnm"
+                    valueField="dptcd"
+                  />
+                )}
+              </td>
+              <th>처리담당자</th>
+              <td>
+                {customOptionData !== null && (
+                  <CustomOptionComboBox
+                    name="person"
+                    value={filters.person}
+                    customOptionData={customOptionData}
+                    changeData={filterComboBoxChange}
+                    textField="user_name"
+                    valueField="user_id"
+                  />
+                )}
+              </td>
+            </tr>
+          </tbody>
+        </FilterBox>
+      </FilterContainer>
+      <GridContainer>
+        <GridTitleContainer className="ButtonContainer">
+          <GridTitle>요약정보</GridTitle>
+        </GridTitleContainer>
+        <ExcelExport
+          data={mainDataResult.data}
+          ref={(exporter) => {
+            _export = exporter;
+          }}
+          fileName={getMenuName()}
+        >
+          <Grid
+            style={{ height: isMobile ? mobileheight : webheight }}
+            data={process(
+              mainDataResult.data.map((row) => ({
+                ...row,
+                person: userListData.find(
+                  (items: any) => items.user_id == row.person
+                )?.user_name,
+                [SELECTED_FIELD]: selectedState[idGetter(row)], //선택된 데이터
+              })),
+              mainDataState
+            )}
+            {...mainDataState}
+            onDataStateChange={onMainDataStateChange}
+            dataItemKey={DATA_ITEM_KEY}
+            selectedField={SELECTED_FIELD}
+            selectable={{
+              enabled: true,
+              mode: "single",
+            }}
+            onSelectionChange={onMainSelectionChange}
+            //스크롤 조회 기능
+            fixedScroll={true}
+            total={mainDataResult.total}
+            skip={page.skip}
+            take={page.take}
+            pageable={true}
+            onPageChange={pageChange}
+            //정렬기능
+            sortable={true}
+            onSortChange={onMainSortChange}
+            //컬럼순서조정
+            reorderable={true}
+            //컬럼너비조정
+            resizable={true}
+          >
+            <GridColumn
+              field="person"
+              title="작성자"
+              width="120px"
+              footerCell={mainTotalFooterCell}
+            />
+            <GridColumn field="custnm" title="업체명" width="120px" />
+            <GridColumn field="tot_time" title="합계(시:분)" width="120px" />
+            {TimeList()}
+          </Grid>
+        </ExcelExport>
+      </GridContainer>
+    </>
+  );
+};
+
+export default CM_B1105W;
